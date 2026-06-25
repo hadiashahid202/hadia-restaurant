11fd9100c29022537b594a6ce57f19ec52f296b7
Hadia Shahid <hadiashahid202@gmail.com>
2026-06-25 21:48:13 +0500

    restaurant

--- assets/images/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-  const about = document.querySelector(".deal-items");
-
-  if (about.getBoundingClientRect().top < window.innerHeight - 100) {
-    about.classList.add("active");
-  }
-});
